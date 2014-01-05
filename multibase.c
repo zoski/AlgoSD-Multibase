481974c98c7d40d9d442fe7f67d6c08000140d57
Gael Alberola <zoski@free.fr>
2014-01-05 11:53:01 +0100

    conversion dans la base n

--- multibase.c
+++ multibase.c
@@ -1,39 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 				/***** PROTOTYPES *****/
-void enBase10(int entree, int base) ;
+void enBase(int entree, int base) ;
 void prompt() ;
 int convNum(int entree, int baseE, int baseS) ;
+int enBase10(char* entree, int base);
+int poids(char lettre, int base);
 
 
 
 
 
 
-int main() {
-	char reponse[256];
-	do {
-		prompt();					//affichage >_
-		scanf("%s", reponse);
-		if(reponse!="quit") {
-			printf("%s", reponse);
-			//évalutation de la commande
-		}
-	} while(reponse!="quit");	
+int main(int argc, char *argv[]) {
+	//enBase(0, 36);
+	//prompt();
+	int test = pow(16,1);
+	//int test = enBase10("4", 16);
+	printf("%d\n", test);
+	return 0;
 }
-
-
-
-
-
-
-
 
 				/***** FONCTIONS *****/
 
-/* Convertis en base 10 l'entier rentré dans la base "base"*/
-void enBase10(int entree, int base) {
+/* Convertis en base 'base' l'entier rentré dans la base 10 */
+void enBase(int entree, int base) {
 	char alphabet[36] = 
 	/*chiffres et lettres représentant l'alphabet utilisé pour les différentes bases*/
 		 {'0', '1', '2', '3', '4', '5', '6', '7',
@@ -41,7 +34,7 @@ void enBase10(int entree, int base) {
 		  'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
 		  'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
 		  'W', 'X', 'Y', 'Z'};
-	int sol[64] ;			/* tableau de la solution*/
+	int sol[64] ;			/* tableau de la solution */
 	int index = 0 ;
 	
 	while (entree != 0) {
@@ -53,9 +46,43 @@ void enBase10(int entree, int base) {
 	
 	/* Affichage */
 	printf("\n");					
-	for ( index ; index >=0 ; index--) {	/* on affiche depuis le dernier enregistré */
+	for ( index ; index>=0 ; index--) {	/* on affiche depuis le dernier enregistré */
 		printf("%c", alphabet[sol[index]]);
 	}
+}
+
+/*			*/
+int enBase10(char* entree, int base) {
+	int resultat = -3;
+	int taille = strlen(entree);
+	int puissance, sol = 0;
+	while(taille != 0) {
+		
+		
+	
+		resultat = poids(entree[taille], base)*pow((double)base,(double)puissance);	/*multipli du poids de la lettre fois la base^index*/
+		sol = sol + resultat ;
+		taille = taille - 1;
+		puissance ++ ;
+	}
+	return sol;
+}
+
+
+/* VERIFIé*/
+int poids(char lettre, int base) {
+	int i ;
+	char alphabet[36] = 
+	/*chiffres et lettres représentant l'alphabet utilisé pour les différentes bases*/
+		 {'0', '1', '2', '3', '4', '5', '6', '7',
+		  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+		  'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+		  'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		  'W', 'X', 'Y', 'Z'};
+	for(i=0 ; i<base ; i++) {
+		if(alphabet[i] == lettre) return i ;
+	}
+	return -1 ;
 }
 
 /* Retourne la conversion tout numérique d'un entier dans la base baseE en un nouveau entier sortie dans la baseS */
@@ -68,6 +95,20 @@ int convNum(int entree, int baseE, int baseS) {
 }
 
 void prompt() {
-	printf("\n> ");
+	char reponse[256];
+	char quit[] = "quit";
+	int compa ;
+	
+	do {
+		printf("\n> ");
+
+		scanf("%s", reponse);
+		printf("%s", reponse);	//debug
+		compa = strcmp(reponse, quit);
+		//évalutation de la commande
+		
+		
+	} while(compa!=0) ; 
+	printf("\nAu-revoir...\n") ;
 }
 
